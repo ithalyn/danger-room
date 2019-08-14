8f334291427ec19af18eb70dc627d0d5a3d432ac
Alexander Ross <mr.alexander.ross@gmail.com>
2019-08-14 01:26:03 -0500

    Initial Import For GSE
    Basic sample code. Nothing spectacular but should illustrate an understanding of the basics.

---
+++ Katas.NumberNames.Test/TranslatorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+namespace Katas.NumberNames.Test {
+    [TestFixture]
+    public class TranslatorTests {
+        private Translator _subject;
+        public TranslatorTests() {
+            _subject = new Translator();
+        }
+        [TestCase(-1)]
+        [TestCase(-2)]
+        [TestCase(-1234)]
+        public void GetNumberName_ValueLessThanZero_ThrowsException(int value) {
+            Assert.Throws<ArgumentException>(() => _subject.GetNumberName(value));
+        }
+
+        [Test]
+        public void GetNumberName_ValueIs0_ReturnsZero() {
+            Assert.AreEqual(expected: "Zero", actual: _subject.GetNumberName(0));
+        }
+
+        [TestCase(1, "One")]
+        [TestCase(2, "Two")]
+        [TestCase(3, "Three")]
+        [TestCase(5, "Five")]
+        [TestCase(9, "Nine")]
+        [TestCase(10, "Ten")]
+        [TestCase(11, "Eleven")]
+        [TestCase(12, "Twelve")]
+        [TestCase(13, "Thirteen")]
+        [TestCase(14, "Fourteen")]
+        [TestCase(15, "Fifteen")]
+        [TestCase(20, "Twenty")]
+        [TestCase(26, "Twenty Six")]
+        [TestCase(37, "Thirty Seven")]
+        [TestCase(48, "Forty Eight")]
+        [TestCase(59, "Fifty Nine")]
+        [TestCase(60, "Sixty")]
+        [TestCase(71, "Seventy One")]
+        [TestCase(82, "Eighty Two")]
+        [TestCase(93, "Ninety Three")]
+        [TestCase(103, "One Hundred and Three")]
+        [TestCase(307, "Three Hundred and Seven")]
+        [TestCase(1234, "One Thousand Two Hundred and Thirty Four")]
+        [TestCase(105034, "One Hundred and Five Thousand and Thirty Four")]
+        [TestCase(437500, "Four Hundred and Thirty Seven Thousand Five Hundred")]
+        [TestCase(7654321, "Seven Million Six Hundred and Fifty Four Thousand Three Hundred and Twenty One")]
+        [TestCase(9999999, "Nine Million Nine Hundred and Ninety Nine Thousand Nine Hundred and Ninety Nine")]
+        public void GetNumberName_CanTranslate(int value, string expected) {
+            Assert.AreEqual(expected: expected, actual: _subject.GetNumberName(value));
+        }
+
+        [TestCase(1000000000)]
+        public void GetNumberName_CanNotTranslatOverOverMillions(int value) {
+            Assert.Throws<ArgumentException>(() => _subject.GetNumberName(value));
+        }
+    }
+}
---
+++ Katas.NumberNames/Translator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Katas.NumberNames {
+    public class Translator {
+        public Translator() {
+        }
+
+        protected string[] _baseNumbers = new[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        protected string[] _tens = new[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+        Dictionary<int, string> _bounds = new Dictionary<int, string>() { { 1000000, "Million" },{ 1000, "Thousand" }, { 100, "Hundred" } };
+
+        public string GetNumberName(int number) {
+            if (number < 0) {
+                throw new ArgumentException("Negative numbers are not allowed.");
+            }
+            else if (number >= 1000000000) {
+                throw new ArgumentException("Values of one billion and above are not allowed.");
+            }
+            else if (number == 0) {
+                return "Zero";
+            }
+
+            string name = string.Empty;
+
+            foreach (var bound in _bounds) {
+                if ((number / bound.Key) > 0) {
+                    if (name != string.Empty) { name += " "; }
+                    name += $"{GetNumberName(number / bound.Key)} {bound.Value}";
+                    number %= bound.Key;
+                }
+            }
+
+            if (number > 0) {
+                if (name != "" && number < 100)
+                    name += " and ";
+
+                if (number < 20)
+                    name += _baseNumbers[number];
+                else {
+                    name += _tens[number / 10];
+                    if ((number % 10) > 0)
+                        name += " " + _baseNumbers[number % 10];
+                }
+            }
+
+            return name;
+        }
+    }
+}
